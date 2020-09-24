3b8b03e9adbb5b6a390a4c08292b084731a30557
Swapnil-Sagar <swapnil.swapnil.sagar946@gmail.com>
2020-09-24 22:52:22 +0530

    changed project section and fixed resume download to target-blank

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const splash = document.querySelector(".splash");
 document.addEventListener("DOMContentLoaded", (e) => {
   setTimeout(() => {
     splash.classList.add("display-none");
-  }, 2000);
+  }, 0);
 });
 
 particlesJS(
